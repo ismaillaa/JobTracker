1e8d2c809a5c903eb45fb32cc2e4f2c400854080
ismaillaa <ismaillaa2007@gmail.com>
2026-04-12 02:02:44 +0100

    add appsettings.Developement.json to gitignore

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,12 @@
-﻿namespace JobTracker.Data
+﻿using JobTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobTracker.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Job> Jobs { get; set; }
     }
 }
