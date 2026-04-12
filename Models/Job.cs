463e30559c3c5c6b024f19f6580092df743cd4eb
ismaillaa <ismaillaa2007@gmail.com>
2026-04-12 02:05:27 +0100

    add appsettings.Developement.json to gitignore

--- Models/Job.cs
+++ Models/Job.cs
@@ -2,5 +2,11 @@
 {
     public class Job
     {
+        public int Id { get; set; }
+        public string Company { get; set; } = string.Empty;
+        public string Position { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public DateTime AppliedAt { get; set; } = DateTime.UtcNow;
+        public string? Notes { get; set; }
     }
 }
